326b5aad83c0ef96e71f5321d188394ed5ada0ab
Chew <chew@chew.pw>
2020-03-11 23:22:01 -0500

    first file!

---
+++ src/pw/chew/transmuteit/TransmuteIt.java
@@ -0,0 +1,17 @@
+package pw.chew.transmuteit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TransmuteIt extends JavaPlugin {
+  // Fired when plugin is first enabled
+  public void onEnable() {
+    FileConfiguration config = this.getConfig();
+    saveConfig();
+
+    System.out.println("[TransmuteIt] Booted!");
+  }
+  // Fired when plugin is disabled
+  public void onDisable() {
+
+  }
+}
