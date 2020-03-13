b39ee1c3b41d4ba31d35aac36eddcf98667a20b2
Chew <chew@chew.pw>
2020-03-13 11:12:45 -0500

    actually get emc value from file

--- src/pw/chew/transmuteit/GetEMCCommand.java
+++ src/pw/chew/transmuteit/GetEMCCommand.java
@@ -8,6 +8,8 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.Material;
 
 public class GetEMCCommand implements CommandExecutor {
+
+
   // This method is called, when somebody uses our command
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (sender instanceof Player) {
@@ -16,7 +18,15 @@ public class GetEMCCommand implements CommandExecutor {
       ItemStack item = inventory.getItemInMainHand​();
       int amount = item.getAmount();
       Material type = item.getType();
-      sender.sendMessage("That item is " + amount + " of " + type.toString());
+      try {
+        int emc = TransmuteIt.json.getInt(type.toString());
+        sender.sendMessage("That item is " + amount + " of " + type.toString() + ". EMC Value: " + emc * amount);
+      } catch(org.json.JSONException e) {
+        sender.sendMessage("That item is " + amount + " of " + type.toString() + ". It has no set EMC value!");
+      }
+
+
+
 
     } else {
       sender.sendMessage("[TransmuteIt] Only players may run this command.");
