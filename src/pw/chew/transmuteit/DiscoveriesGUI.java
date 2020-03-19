617646f9d927dc4a7b694ab5b47134851d62a571
Chew <chew@chew.pw>
2020-03-19 14:33:10 -0500

    fix minor exploit but not really an exploit, still fixed tho

--- src/pw/chew/transmuteit/DiscoveriesGUI.java
+++ src/pw/chew/transmuteit/DiscoveriesGUI.java
@@ -74,27 +74,48 @@ public class DiscoveriesGUI implements InventoryHolder, Listener {
     // verify current item is not null
     if (clickedItem == null || clickedItem.getType() == Material.AIR) return;
 
+    if(e.getRawSlot() > 53) {
+      return;
+    }
+
     UUID uuid = player.getUniqueId();
     String name = clickedItem.getType().toString();
     int amount = 1;
 
-    int emc = new DataManager().getEMC(uuid, player);
-    int value;
-    try {
-      value = TransmuteIt.json.getInt(name);
-    } catch(org.json.JSONException f) {
-      player.sendMessage("This item no longer has an EMC value!");
-      return;
-    }
-    if((value * amount) > emc) {
-      player.sendMessage("You don't have enough EMC!");
-    }
+    if(new DataManager().discovered(uuid, name)) {
+      int emc = new DataManager().getEMC(uuid, player);
+      int value;
+      try {
+        value = TransmuteIt.json.getInt(name);
+      } catch(org.json.JSONException f) {
+        player.sendMessage("This item no longer has an EMC value!");
+        return;
+      }
+      if((value * amount) > emc) {
+        player.sendMessage("You don't have enough EMC!");
+        return;
+      }
 
-    PlayerInventory inventory = player.getInventory();
-    ItemStack item = new ItemStack(Material.getMaterial(name), amount);
-    inventory.addItem(item);
-    DataManager bob = new DataManager();
-    bob.writeEMC(uuid, emc - (value * amount), player);
-    player.sendMessage("Successfully transmuted " + (value * amount) + " EMC into " + amount + " " + name);
+      PlayerInventory inventory = player.getInventory();
+      ItemStack item = new ItemStack(Material.getMaterial(name), amount);
+      inventory.addItem(item);
+      DataManager bob = new DataManager();
+      bob.writeEMC(uuid, emc - (value * amount), player);
+      player.sendMessage("Successfully transmuted " + (value * amount) + " EMC into " + amount + " " + name);
+    } else {
+      try {
+        DataManager bob = new DataManager();
+        if(bob.discovered(uuid, name) == false) {
+          player.sendMessage("You've discovered " + name + "!");
+          if(bob.discoveries(uuid).size() == 0) {
+            player.sendMessage("Now you can run /transmute get " + name + " [amount] to get this item, given you have enough EMC!");
+          }
+          new DataManager().writeDiscovery(uuid, name);
+        }
+      // If there's no JSON file or it's not IN the JSON file
+      } catch(org.json.JSONException f) {
+        player.sendMessage("This item has no set EMC value!");
+      }
+    }
   }
 }
