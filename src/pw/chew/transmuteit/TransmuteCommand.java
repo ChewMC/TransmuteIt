01b7ee5ad796175ff2ed0e76815b892a41e4ae16
Chew <chew@chew.pw>
2020-03-25 21:42:54 -0500

    [Fixes #15] Add /transmute analyze because why not

--- src/pw/chew/transmuteit/TransmuteCommand.java
+++ src/pw/chew/transmuteit/TransmuteCommand.java
@@ -9,13 +9,11 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.util.StringUtil;
 
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class TransmuteCommand implements CommandExecutor, TabCompleter {
 
@@ -149,23 +147,23 @@ public class TransmuteCommand implements CommandExecutor, TabCompleter {
             }
           }
         } else if(arg0.equals("learn")) {
-          PlayerInventory inventory = ((Player)sender).getInventory();
+          PlayerInventory inventory = ((Player) sender).getInventory();
           ItemStack item = inventory.getItemInMainHand();
           Material type = item.getType();
           String name = type.toString();
           // If it's nothing
-          if(name.equals("AIR")) {
+          if (name.equals("AIR")) {
             sender.sendMessage("Please hold an item to learn it!");
           } else {
             // If it's something
             try {
               TransmuteIt.json.getInt(type.toString());
               DataManager bob = new DataManager();
-              UUID uuid = ((Player)sender).getUniqueId();
+              UUID uuid = ((Player) sender).getUniqueId();
               sender.sendMessage(ChatColor.COLOR_CHAR + "d--------[ " + ChatColor.COLOR_CHAR + "bTransmuting Stats" + ChatColor.COLOR_CHAR + "d ]--------");
-              if(!bob.discovered(uuid, name)) {
+              if (!bob.discovered(uuid, name)) {
                 sender.sendMessage(ChatColor.COLOR_CHAR + "aYou've discovered " + name + "!");
-                if(bob.discoveries(uuid).size() == 0) {
+                if (bob.discoveries(uuid).size() == 0) {
                   sender.sendMessage(ChatColor.COLOR_CHAR + "7" + ChatColor.ITALIC + "Now you can run /transmute get " + name + " [amount] to get this item, given you have enough EMC!");
                 }
                 new DataManager().writeDiscovery(uuid, name);
@@ -173,11 +171,57 @@ public class TransmuteCommand implements CommandExecutor, TabCompleter {
                 sender.sendMessage(ChatColor.COLOR_CHAR + "cYou've already discovered " + name + "!");
               }
               return true;
-            // If there's no JSON file or it's not IN the JSON file
-            } catch(org.json.JSONException e) {
+              // If there's no JSON file or it's not IN the JSON file
+            } catch (org.json.JSONException e) {
               sender.sendMessage("This item has no set EMC value!");
             }
           }
+        } else if(arg0.equals("analyze")) {
+          PlayerInventory inventory = ((Player) sender).getInventory();
+          HashMap<String, Integer> amountMap = new HashMap<>();
+          HashMap<String, Integer> emcValueMap = new HashMap<>();
+          for (int i = 0; i < inventory.getSize(); i++) {
+            try {
+              ItemStack item = inventory.getItem(i);
+              String name = item.getType().toString();
+              if (amountMap.containsKey(name)) {
+                int current = amountMap.get(name);
+                amountMap.replace(name, current + item.getAmount());
+              } else {
+                amountMap.put(name, item.getAmount());
+              }
+              int emc = -1;
+              try {
+                emc = TransmuteIt.json.getInt(name);
+              } catch(org.json.JSONException ignored) {
+
+              }
+              if(item.getItemMeta() instanceof Damageable) {
+                Damageable damage = ((Damageable) item.getItemMeta());
+                emcValueMap.put(name, damage.getDamage() * emc);
+              } else {
+                emcValueMap.put(name, emc);
+              }
+            } catch(NullPointerException ignored) {
+
+            }
+          }
+          sender.sendMessage(ChatColor.COLOR_CHAR + "d--------[ " + ChatColor.COLOR_CHAR + "bInventory Analysis" + ChatColor.COLOR_CHAR + "d ]--------");
+          Object[] keys = amountMap.keySet().toArray();
+          Arrays.sort(keys);
+          int total = 0;
+          for(int i = 0; i < keys.length; i++) {
+            String name = (String) keys[i];
+            int amount = amountMap.get(name);
+            try {
+              int emc = TransmuteIt.json.getInt(name);
+              sender.sendMessage(ChatColor.YELLOW + capitalize(name) + ": " + ChatColor.GREEN + NumberFormat.getInstance().format(emc * amount) + " EMC (" + NumberFormat.getInstance().format(emc) + " EMC each for " + amount + " items)");
+              total += emc * amount;
+            } catch(org.json.JSONException e) {
+              sender.sendMessage(ChatColor.YELLOW + capitalize(name) + ": " + ChatColor.GREEN + "No EMC Value!");
+            }
+          }
+          sender.sendMessage(ChatColor.YELLOW + "TOTAL" + ": " + ChatColor.GREEN + NumberFormat.getInstance().format(total) + " EMC");
         } else {
           sender.sendMessage("Invalid subcommand! Need help? Try \"/transmute help\"");
         }
@@ -202,6 +246,7 @@ public class TransmuteCommand implements CommandExecutor, TabCompleter {
       commands.add("get");
       commands.add("take");
       commands.add("learn");
+      commands.add("analyze");
       StringUtil.copyPartialMatches(args[0], commands, completions);
     } else if (args.length >= 2 && args[0].equals("help")) {
       StringUtil.copyPartialMatches(args[1], commands, completions);
@@ -226,7 +271,7 @@ public class TransmuteCommand implements CommandExecutor, TabCompleter {
       String first = word.substring(0, 1).toUpperCase();
       newword = newword + first + rest + " ";
     }
-    return newword;
+    return newword.substring(0, newword.length()-1);
   }
 
   public void helpResponse(CommandSender sender) {
